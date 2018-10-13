941c0d469eda8f68f6f67045d4e4ca2b35b5a2bc
kristen <kristen.chong.brown@gmail.com>
2018-10-13 10:46:32 -0600

    update e001 solution
    Thanks to a reminder that math can solve these problems too, I updated the e001 solution so that the last 2 test cases now pass

--- c++/hacker-rank/euler+/e001.cpp
+++ c++/hacker-rank/euler+/e001.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 using namespace std;
 
+long summation(int divby, long max){
+  long sum;
+  // multiply by divby to get numbers divisible
+  // by divby. Use max-1 since we are checking numbers
+  // below some max number. then summation formula
+  sum = divby * ((max-1)/divby * ((max-1)/divby +1))/2;
+
+  return sum;
+}
 int main() {
   //get number of numbers to check
   int t;
   cin >> t;
 
-  for (int i = 0; i < t; i++){
-    int n;
+  for (long i = 0; i < t; i++){
+    long n, tot, sum3, sum5, sum15;
     cin >> n;
-    // more generic loop from project-euler solution
-    int tot = 0;
-    for (int j = 0; j<n; j++){
-      if (j%3 == 0 || j%5 ==0){
-        tot += j;
-      }
-    }
+    // sum instead of looping. I forgot math was a thing.
+    sum3 = summation(3,n);
+    sum5 = summation(5,n);
+    sum15 = summation(15,n);
+
+    tot = sum3 + sum5 - sum15;
     cout << tot << endl;
   }
   return 0;
