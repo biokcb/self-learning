5434a15cb959a8fc267d4893f1ce454db140bb11
kristen <kristen.chong.brown@gmail.com>
2018-10-13 13:45:18 -0600

    update e002
    add the mathy-solution with proof

--- c++/hacker-rank/euler+/e002.cpp
+++ c++/hacker-rank/euler+/e002.cpp
@@ -2,22 +2,32 @@
 using namespace std;
 
 long fib(long max){
-  // calculate sum of even fibonacci numbers
-  // initialize starting conditions
-  long sum;
-  int e[3] = {1,2,3};
-  int p = 2;
+  /* calculate sum of even fibonacci numbers assuming every 3rd number is even
+  fibonacci sum proof as practice (obvious could just copy the solution, 
+  but good to understand the math.
 
-  while (e[2] <= max){
-    if (e[2]%2 == 0){
-      p += e[2];
-    }
-    //shift numbers and sum
-    e[0] = e[1];
-    e[1] = e[2];
-    e[2] = e[0] + e[1];
+  if fib(max) is even, then fib(max-1) & fib(max-2) are odd & fib(max-3) and fib(max-6) are even
+  fib(max) = fib(max-1) + fib(max-2)
+           = fib(max-1-1) + fib(max-1-2) + fib(max-2-1) + fib(max-2-2)
+           = 2*fib(max-3) + fib(max-2) + fib(max-4)
+           = 2*fib(max-3) + fib(max-2-1)+fib(max-2-2) + fib(max-4-1)+fib(max-4-2)
+           = 3*fib(max-3) + fib(max-4) + fib(max-5) + fib(max-6)
+           = 3*fib(max-3) + [ fib(max-3-1) + fib(max-3-2) = fib(max-3) ] + fib(max-6)
+           = 4*fib(max-3) + fib(max-6)
+  fib(21) = 4*5 + 1 = 21, fib(34) = 4*8+2 =34 which can also be evenfib(34)=evenfib(34-1)+evenfib(34-2)
+  */
+  long currmax = 8;
+  long priormax = 2;
+  long sum = 2;
+  long newmax;
+
+  while (currmax <= max){
+      sum += currmax;
+      newmax = 4*(currmax) + priormax;
+      priormax = currmax;
+      currmax = newmax;
   }
-  return p;
+  return sum;
 }
 
 int main() {
